4039e2dad1903cd25435c89f2e824554d7ecbe45
M.S.Odoevsky <odoevsky.maksim@urfu.ru>
2017-11-08 13:02:12 +0500

    house-page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     $('.drawer').drawer();
 });
+
+$('.single-item').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.single-item-no-autoplay').slick({
+    dots: true,
+    autoplay: false,
+    fade: true,
+    cssEase: 'linear'
+});
